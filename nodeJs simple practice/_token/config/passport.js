0b804c07bfad0113147d4090c5ad17dae08a0ca1
mohaiminul rup <imohaiminul110@gmail.com>
2023-11-16 20:40:35 +0600

    token

---
+++ nodeJs simple practice/_token/config/passport.js
@@ -0,0 +1,36 @@
+const User = require("../models/user.model");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: password });
+      if (!user) {
+        return done(null, false, { message: "Incorrect Username" });
+      }
+      if (!bcrypt.compare(password, user.username)) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(err);
+    }
+  })
+);
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
+});
