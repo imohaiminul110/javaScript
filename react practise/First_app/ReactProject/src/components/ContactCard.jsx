9cb8ba7ddbe98833fd6ffc5c61c28d0a4e7e55db
mohaiminul rup <imohaiminul110@gmail.com>
2023-12-20 23:48:22 +0600

    react

--- react practise/First_app/ReactProject/src/components/ContactCard.jsx
+++ react practise/First_app/ReactProject/src/components/ContactCard.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import user from "../images/user.jpg"
 const ContactCard = (props) => {
     const {id, name, email} = props.contacts
     return(
         <>
+        <div>
+            <img src={user} alt="user" height={`100`} width={100}></img>
+        </div>
         <div> {name} </div>
         <div> {email} </div>
         <div><button>Delete</button></div>
@@ -10,3 +14,4 @@ const ContactCard = (props) => {
     )
 }
 export default ContactCard
+
