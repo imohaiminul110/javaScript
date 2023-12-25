db46cd5a659b8a04004991b90c84fd856d3c39e2
mohaiminul rup <imohaiminul110@gmail.com>
2023-12-25 23:48:01 +0600

    axiosPractice

--- react practise/First_app/axiosPractice/src/components/Create.jsx
+++ react practise/First_app/axiosPractice/src/components/Create.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Create = () => {
   return (
     <div>
-      <h1>Create</h1>
+      <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-10000' >
+        <div className='w-200 rounded bg-white border shadow p-4'>
+            <h1>Add a user</h1>
+            <form>
+                <div>
+                    <label>Name: </label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>Phone: </label>
+                    <input></input>
+                </div>
+                <button>Submit</button>
+                <Link>Back</Link>
+            </form>
+        </div>
+      </div>
     </div>
   )
 }
