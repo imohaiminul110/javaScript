6ccf208113ee3a82af6cb83af4837ef376428269
mohaiminul rup <imohaiminul110@gmail.com>
2023-12-25 23:55:42 +0600

    axiosPractice

--- react practise/First_app/axiosPractice/src/components/Create.jsx
+++ react practise/First_app/axiosPractice/src/components/Create.jsx
@@ -5,19 +5,19 @@ const Create = () => {
   return (
     <div>
       <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-10000' >
-        <div className='w-200 rounded bg-white border shadow p-4'>
+        <div className='w-200 rounded bg-white  shadow p-4'>
             <h1>Add a user</h1>
             <form>
-                <div>
-                    <label>Name: </label>
-                    <input></input>
+                <div className='mb-2'>
+                    <label htmlFor='name' >Name: </label>
+                    <input type='text' name='name' className='form-control' placeholder='Enter Name' ></input>
                 </div>
-                <div>
-                    <label>Phone: </label>
-                    <input></input>
+                <div className='mb-2'>
+                    <label htmlFor='phone' >Phone: </label>
+                    <input type='text' name='phone' className='form-control' placeholder='Enter Phone' ></input>
                 </div>
-                <button>Submit</button>
-                <Link>Back</Link>
+                <button className='btn btn-success' >Submit</button>
+                <Link to="/" className='btn btn-primary ms-3' >Back</Link>
             </form>
         </div>
       </div>
